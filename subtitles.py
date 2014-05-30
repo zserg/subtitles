40e7f232dae7e8aa0e985808b3c7210c27d06986
ZSerg <zsergt3@gmail.com>
2014-05-30 18:14:08 +0400

    ...

--- subtitles.py
+++ subtitles.py
@@ -9,13 +9,14 @@ import time
 show_lines = 10
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 if len(sys.argv)!= 3 :
    print "usage: subtitles.py <file_ru> <file_eng>"
    exit()
 
 file_ru = open(sys.argv[1]);
+file_en = open(sys.argv[2]);
 
 
 r_size = 0;
@@ -46,6 +47,24 @@ for line in file_ru:
 
 time_code = time.strptime(time_code_s,"%H:%M:%S")
 
+time_code_not_found = True
+for line in file_en:
+    if time_code_not_found:
+        sObj = re.search( r'(\d+\:\d+\:\d+),', line, re.M|re.I)
+        if sObj:
+            time_code_s = sObj.group(1)
+            time_code_en = time.strptime(time_code_s,"%H:%M:%S")
+            if time_code == time_code_en:
+               logging.debug(time_code)
+               logging.debug(time_code_en)
+               time_code_not_found = False
+               wait = raw_input("Press ENTER for english...")
+               print "==================================="
+               print line
+               for i in range(10):
+                   print next(file_en).rstrip()
+
+    
 
 
 
